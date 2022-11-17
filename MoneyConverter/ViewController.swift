b4269f4d506dd7b164342e8c198be828279b8bfa
Dmitry Meshechkov <d.meshechkov@yandex.ru>
2022-11-18 00:48:00 +0500

    Finished the App disign, and added IBOutlet & IBAction

--- MoneyConverter/ViewController.swift
+++ MoneyConverter/ViewController.swift
@@ -9,11 +9,46 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var inputPrice: UITextField!
+    @IBOutlet weak var labelUsd: UILabel!
+    @IBOutlet weak var labelRub: UILabel!
+    
+    @IBOutlet weak var slider: UISlider! {
+        didSet {
+            slider.maximumValue = 3000
+            slider.minimumValue = 0
+            slider.value = 0
+        }
     }
-
-
+    
+    
+    
+    var price: Float = 0
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//
+//        guard let price = self.price else {return}
+//        inputPrice.text = "\(price)"
+//
+//    }
+    
+    
+    
+    @IBAction func sliderChanger(_ sender: UISlider) {
+        price = Float(inputPrice.text!)!
+        
+        let valueUsd = Float(sender.value)
+        labelUsd.text = "\(valueUsd) USD"
+        
+        let valueRub = Float(sender.value * Float(price))
+        labelRub.text = "\(valueRub) RUB"
+    }
+    
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
 }
+
 
